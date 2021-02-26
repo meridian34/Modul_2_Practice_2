d30ebe92dc52af2fbca3564add36406a2baa094f
meridian34 <sobol.andrey.25@gmail.com>
2021-02-26 21:48:02 +0200

    Добавьте файлы проекта.

---
+++ Modul_2_Practice_2/Model/BiologicalKingdom/KingdomAnimalia/TypeOfAnimal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Modul_2_Practice_2.Model.Enum;
+
+namespace Modul_2_Practice_2.Model.BiologicalKingdom.KingdomAnimalia
+{
+    public abstract class TypeOfAnimal : BiologicalKingdom
+    {
+        public TypeOfAnimal()
+        {
+            Kingdom = Enum.BiologicalKingdom.KingdomAnimalia;
+        }
+
+        public override Enum.BiologicalKingdom Kingdom { get; set; }
+
+        public abstract Enum.TypeOfAnimal Type { get; set; }
+    }
+}
