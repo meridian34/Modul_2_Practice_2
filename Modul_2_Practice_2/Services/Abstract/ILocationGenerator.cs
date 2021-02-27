439fda4607ee4e18ad929cbfdabdbf3e2cae1961
meridian34 <sobol.andrey.25@gmail.com>
2021-02-27 09:32:49 +0200

    Revert "Добавьте файлы проекта."
    This reverts commit d30ebe92dc52af2fbca3564add36406a2baa094f.

--- Modul_2_Practice_2/Services/Abstract/ILocationGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿using Modul_2_Practice_2.Model;
-
-namespace Modul_2_Practice_2.Services.Abstract
-{
-    public interface ILocationGenerator
-    {
-        Location GenerateLocation(int maxAnimal);
-    }
-}
