7c29eb6e86ebd7cb46fc609759f903ddf59fe087
meridian34 <sobol.andrey.25@gmail.com>
2021-02-27 09:40:27 +0200

    Revert "Revert "Добавьте файлы проекта.""
    This reverts commit 439fda4607ee4e18ad929cbfdabdbf3e2cae1961.

---
+++ Modul_2_Practice_2/Model/BiologicalKingdom/KingdomAnimalia/AnimalClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul_2_Practice_2.Model.BiologicalKingdom.KingdomAnimalia
+{
+    public abstract class AnimalClass : TypeOfAnimal
+    {
+        public AnimalClass()
+        {
+            Type = Enum.TypeOfAnimal.Chordates;
+        }
+
+        public override Enum.TypeOfAnimal Type { get; set; }
+
+        public abstract Enum.AnimalClass Class { get; set; }
+    }
+}
---
+++ Modul_2_Practice_2/Model/Enum/SquadOfAnimal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul_2_Practice_2.Model.Enum
+{
+    public enum SquadOfAnimal
+    {
+        None,
+        ClovenFooted,
+        Primates,
+        Predatory,
+        Proboscidean,
+        Crocodile,
+        Scaly,
+        CraneLike,
+        Parrot,
+        HawkLike,
+        Cichliformes
+    }
+}
